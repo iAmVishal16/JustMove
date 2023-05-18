e96be09394d9a32f84ac624d1738b288ad754b3a
Vishal Paliwal <vishal.paliwal@ril.com>
2023-05-18 23:25:44 +0530

    Initial Commit

---
+++ JustMove/JustMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustMoveApp.swift
+//  JustMove
+//
+//  Created by Vishal Paliwal on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct JustMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
